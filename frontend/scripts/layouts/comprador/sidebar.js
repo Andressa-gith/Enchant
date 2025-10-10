e4916630d50eb70db1470dc4a6b8d710a9ef7fa5
Andressa-gith <andressa.cardoso@ba.estudante.senai.br>
2025-10-10 14:19:25 -0300

    SideBar

--- frontend/scripts/layouts/comprador/sidebar.js
+++ frontend/scripts/layouts/comprador/sidebar.js
@@ -151,7 +151,7 @@ class SidebarManager {
             .sidebar-nav span { font-size: 14px; opacity: 0; visibility: hidden; transition: opacity 0.2s, visibility 0.2s; }
             .sidebar-nav i { font-size: 1.2rem; margin-right: 20px; color: #4e4e4e; min-width: 20px; text-align: center; }
             /* EFEITO HOVER E ACTIVE APLICADO AQUI */
-            .sidebar-nav a:hover, .sidebar-nav a.active { background-color: #caae8d; color: black; }
+            .sidebar-nav a:hover, .sidebar-nav a.active { background-color:rgb(209, 209, 209); color: black; }
             .sidebar-nav a:hover i, .sidebar-nav a.active i { color: black; }
             .main-content { margin-left: var(--sidebar-collapsed); transition: var(--transition); }
             .sidebar-overlay { position: fixed; top: 0; left: 0; right: 0; bottom: 0; background-color: rgba(0,0,0,0.7); z-index: 799; display: none; opacity: 0; transition: opacity 0.3s ease; }
