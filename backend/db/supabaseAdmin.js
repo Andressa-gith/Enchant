cdfcf8efe5b32c37865c2ef3eb723d71a57b1360
Guilherme Oliver <guilherme.oliver@msn.com>
2025-10-11 11:31:33 -0300

    d

--- backend/db/supabaseAdmin.js
+++ backend/db/supabaseAdmin.js
@@ -2,8 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { createClient } from '@supabase/supabase-js';
 
-// Este cliente usa a SERVICE_KEY e tem poderes de administrador.
-// Use-o APENAS no back-end para tarefas que exigem privilégios elevados.
 const supabaseAdmin = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_KEY,
