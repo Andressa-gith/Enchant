0e310d8a075a37b3cc3f7abfc9b93d441ebc9b86
Guilherme Oliver <guilherme.oliver@msn.com>
2025-10-13 15:39:51 -0300

    #bill

--- backend/routes/mercadoPago.routes.js
+++ backend/routes/mercadoPago.routes.js
@@ -1,17 +1,18 @@
-// routes/mercadoPago.routes.js
 import express from 'express';
-import { generateAuthLink, handleCallback, disconnect } from '../controllers/mercadoPago.controller.js';
+import { 
+    generateAuthLink,
+    handleCallback, 
+    disconnect 
+} from '../controllers/mercadoPago.controller.js';
 import { protegerRota } from '../middleware/auth.middleware.js'; // Use seu middleware de autenticação
 
 const mercadoPagoRoutes = express.Router();
 
-// Rota para iniciar a conexão. A ONG será redirecionada para o Mercado Pago
+// Rotas
 mercadoPagoRoutes.get('/authorize', generateAuthLink);
 
-// Rota para onde o Mercado Pago redirecionará a ONG após a autorização
 mercadoPagoRoutes.get('/callback', handleCallback);
 
-//opcional
 mercadoPagoRoutes.post('/disconnect', protegerRota, disconnect);
 
 export default mercadoPagoRoutes;
